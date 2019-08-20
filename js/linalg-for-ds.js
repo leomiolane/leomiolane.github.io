bcd3960b3e10203ebb1d632a26e85708d4afacb7
leomiolane <leo.miolane@gmail.com>
2019-08-19 17:03:14 -0700

    close menu on click

--- js/linalg-for-ds.js
+++ js/linalg-for-ds.js
@@ -1,25 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  // Get all "navbar-burger" elements
-  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+	// Get all "navbar-burger" elements
+	const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+	const $navbarClickers = Array.prototype.slice.call(document.querySelectorAll('.navbar-clicker'), 0);
 
-  // Check if there are any navbar burgers
-  if ($navbarBurgers.length > 0) {
 
-    // Add a click event on each of them
-    $navbarBurgers.forEach( el => {
-      el.addEventListener('click', () => {
+	// Add a click event on each of them
+	$navbarBurgers.forEach( el => {
+		// Get the target from the "data-target" attribute
+		const target = el.dataset.target;
+		const $target = document.getElementById(target);
+		el.addEventListener('click', () => {
+			// Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+			el.classList.toggle('is-active');
+			$target.classList.toggle('is-active');
 
-        // Get the target from the "data-target" attribute
-        const target = el.dataset.target;
-        const $target = document.getElementById(target);
+		});
+		$navbarClickers.forEach( cli => {
+			cli.addEventListener('click', () => {
+				// Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+				el.classList.remove('is-active');
+				$target.classList.remove('is-active');
 
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-        el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
-
-      });
-    });
-  }
+			});
+		});
+	});
 
 });
